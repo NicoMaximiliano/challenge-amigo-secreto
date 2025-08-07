15a7302cb60faa74d0048b25c61a4cf7083d2d9e
NicoMaximiliano <nicolas.orellano25@outlook.com.ar>
2025-08-07 17:20:40 -0300

    Se agrego una nueva funcion para elegir de forma aleatoria a un amigo

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ let amigos = [];
 function agregarAmigo() {
   // Agrega el nombre del amigo al arreglo "amigos"
   let nombre = document.getElementById("amigo").value;
-  //   console.log(nombre);
-  //   console.log(nombre.toUpperCase());
   nombre = nombre.toUpperCase(); // Convierte el nombre a mayúsculas
+
   if (validarNombreDeAmigo(nombre)) {
     amigos.push(nombre);
     agregarAmigoALaLista(nombre);
@@ -28,6 +27,7 @@ function validarNombreDeAmigo(nombre) {
 function limpiarCampoDeEntrada() {
   // Limpia el campo de entrada del nombre del amigo
   document.getElementById("amigo").value = "";
+  document.getElementById("amigo").focus(); // Enfoca el campo de entrada después de limpiar
 }
 
 function agregarAmigoALaLista(amigo) {
@@ -37,4 +37,17 @@ function agregarAmigoALaLista(amigo) {
   let li = document.createElement("li");
   li.textContent = amigo;
   lista.appendChild(li);
+}
+
+function sortearAmigo() {
+  // Sortea un amigo al azar del arreglo "amigos" y muestra su nombre en el HTML
+  if (amigos.length === 0) {
+    alert("No hay amigos en la lista para sortear.");
+    return;
+  }
+  let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+  let amigoSorteado = amigos[indiceAleatorio];
+  document.getElementById("listaAmigos").innerHTML = ""; // Limpiar la lista antes de mostrar el amigo sorteado
+  document.getElementById("resultado").innerHTML = amigoSorteado;
+  console.log("Amigo sorteado: " + amigoSorteado);
 }
